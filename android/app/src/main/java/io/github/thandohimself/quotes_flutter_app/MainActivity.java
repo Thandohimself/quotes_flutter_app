44489a23130789f3ddafaff2116cca37d68d1275
Thandohimself <thandohimself@gmail.com>
2019-12-02 23:06:33 +0200

    Initial commit

---
+++ android/app/src/main/java/io/github/thandohimself/quotes_flutter_app/MainActivity.java
@@ -0,0 +1,13 @@
+package io.github.thandohimself.quotes_flutter_app;
+
+import android.os.Bundle;
+import io.flutter.app.FlutterActivity;
+import io.flutter.plugins.GeneratedPluginRegistrant;
+
+public class MainActivity extends FlutterActivity {
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    GeneratedPluginRegistrant.registerWith(this);
+  }
+}
