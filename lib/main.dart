5a9fc28667c1f7e112e627c2decb291bb56cc27c
Thandohimself <thandohimself@gmail.com>
2019-12-02 23:29:35 +0200

    Displaying Lists

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,35 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+  home: QuoteList(),
+));
+
+class QuoteList extends StatefulWidget {
+  @override
+  _QuoteListState createState() => _QuoteListState();
+}
+
+class _QuoteListState extends State<QuoteList> {
+  List<String> quotes = [
+    'The only thing that comes to a sleeping man is dreams.',
+    'During your life, never stop dreaming. ',
+    'Is it a crime, to fight, for what is mine?'
+
+  ];
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white70,
+      appBar: AppBar(
+        title: Text('Great Quotes'),
+        centerTitle: true,
+        backgroundColor: Colors.deepOrangeAccent[100],
+      ),
+      body: Column(
+        children: quotes.map((quote) {
+          return Text(quote);
+        }).toList(),
+      ),
+    );
+  }
+}
