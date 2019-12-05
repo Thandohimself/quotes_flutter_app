e66ec90c2d879ecaea0f9480dee77b5d916aa290
Thandohimself <thandohimself@gmail.com>
2019-12-05 22:06:20 +0200

    used custom Class objects created custom Widget (cardview) populated cardview

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'quote.dart';
 
 void main() => runApp(MaterialApp(
-  home: QuoteList(),
-));
+      home: QuoteList(),
+    ));
 
 class QuoteList extends StatefulWidget {
   @override
@@ -12,11 +12,37 @@ class QuoteList extends StatefulWidget {
 
 class _QuoteListState extends State<QuoteList> {
   List<Quote> quotes = [
- Quote(author: 'Bob', text: 'The only thing that comes to a sleeping man is dreams.'),
+    Quote(
+        author: 'Bob',
+        text: 'The only thing that comes to a sleeping man is dreams.'),
     Quote(author: 'Max', text: 'During your life, never stop dreaming. '),
     Quote(author: 'Pac', text: 'Is it a crime, to fight, for what is mine?')
-
   ];
+
+  Widget quoteTemplate(quote) {
+    return Card(
+      margin: EdgeInsets.fromLTRB(16, 16, 16, 0),
+      child: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              quote.text,
+              style: TextStyle(fontSize: 18.0, color: Colors.grey[600]),
+            ),
+            SizedBox(
+              height: 6.0,
+            ),
+            Text(
+              quote.author,
+              style: TextStyle(fontSize: 14.0, color: Colors.grey[800]),
+            )
+          ],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -28,7 +54,7 @@ class _QuoteListState extends State<QuoteList> {
         backgroundColor: Colors.deepOrangeAccent[100],
       ),
       body: Column(
-        children: quotes.map((quote) => Text('${quote.text} - ${quote.author} ')).toList(),
+        children: quotes.map((quote) => quoteTemplate(quote)).toList(),
       ),
     );
   }
